849051ec06243e9e0cb6c4cc53de3a683198c4fa
Chris Petkau <chris.petkau@kleientertainment.com>
2022-09-07 10:07:18 -0700

    moonlander_colemak_coder_rlXzn_XGXaP.zip

--- keyboards/moonlander/keymaps/chrispetkau/config.h
+++ keyboards/moonlander/keymaps/chrispetkau/config.h
@@ -7,8 +7,9 @@
 #define PERMISSIVE_HOLD
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"rlXzn/weRDd"
+#define FIRMWARE_VERSION u8"rlXzn/XGXaP"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
 #define RGB_MATRIX_STARTUP_SPD 60
 #include "petkau_config.inl"
